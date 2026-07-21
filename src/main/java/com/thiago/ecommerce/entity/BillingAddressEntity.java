814d3f72dd4eff0238a586dd62ef80130a20a6c3
Thiago Correia <cor.thiago@gmail.com>
2026-07-21 05:23:19 -0300

    feat: add UserEntity and BillingAdressEntity

---
+++ src/main/java/com/thiago/ecommerce/entity/BillingAddressEntity.java
@@ -0,0 +1,66 @@
+package com.thiago.ecommerce.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "billing_address")
+public class BillingAddressEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "billing_address_id")
+    private Long billingAddressId;
+
+    @Column(name = "street")
+    private String street;
+
+    @Column(name = "number")
+    private String number;
+
+    @Column(name = "complement")
+    private String complement;
+
+    @OneToOne(mappedBy = "billingAddress")
+    private UserEntity user;
+
+    public BillingAddressEntity(){}
+
+    public Long getBillingAddressId() {
+        return billingAddressId;
+    }
+
+    public void setBillingAddressId(Long billingAddressId) {
+        this.billingAddressId = billingAddressId;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public String getComplement() {
+        return complement;
+    }
+
+    public void setComplement(String complement) {
+        this.complement = complement;
+    }
+
+    public UserEntity getUser() {
+        return user;
+    }
+
+    public void setUser(UserEntity user) {
+        this.user = user;
+    }
+}
---
+++ src/main/java/com/thiago/ecommerce/entity/UserEntity.java
@@ -0,0 +1,48 @@
+package com.thiago.ecommerce.entity;
+
+import jakarta.persistence.*;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "users")
+public class UserEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "user_id")
+    private UUID userId;
+
+    @Column(name =  "full_name")
+    private String fullName;
+
+    @OneToOne
+    @JoinColumn(name = "billing_address_id")
+    private BillingAddressEntity billingAddress;
+
+    public UserEntity() {
+    }
+
+    public UUID getUserId() {
+        return userId;
+    }
+
+    public void setUserId(UUID userId) {
+        this.userId = userId;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public BillingAddressEntity getBillingAddress() {
+        return billingAddress;
+    }
+
+    public void setBillingAddress(BillingAddressEntity billingAddress) {
+        this.billingAddress = billingAddress;
+    }
+}
